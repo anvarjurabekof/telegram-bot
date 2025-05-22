c2bf18e85aab4c6e6f8e07b3d38145ae2497e0ad
anvarjurabekof <151896816+anvarjurabekof@users.noreply.github.com>
2025-05-22 23:38:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# /start komandasi funksiyasi
+# /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        ['🇺🇿 O‘zbek', '🇷🇺 Русский']
-    ]
+    keyboard = [['🇺🇿 O‘zbek', '🇷🇺 Русский']]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     await update.message.reply_text(
         "Iltimos, tilni tanlang 👇\nПожалуйста, выберите язык 👇",
         reply_markup=reply_markup
     )
 
-# Botni ishga tushirish
 if __name__ == '__main__':
     import os
     TOKEN = os.environ.get("BOT_TOKEN")
 
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    # Eskirgan getUpdates’larni tashlab, conflict xatosini bartaraf etamiz
+    # Faqat polling, eskirgan updates’larni tashlab ketadi
     app.run_polling(drop_pending_updates=True)
